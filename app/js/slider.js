161ebc2b1bdcfabcc00b15670010afbc5f834b51
Max Lopez <lopezmax.p@gmail.com>
2015-10-25 16:51:37 -0500

    Avance página de Administración

---
+++ app/js/slider.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function ($) {
+  $('.pgwSlideshow').pgwSlideshow({
+  	autoSlide: true,
+  	displayControls: true,
+  	displayList: false,
+  	transitionEffect: 'fade',
+  	intervalDuration: 3000,
+  });
+});
